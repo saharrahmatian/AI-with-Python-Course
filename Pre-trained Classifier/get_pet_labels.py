a31de7b3e31e1cec43d16a854d628a005e91437b
saharrahmatian <saharrahmatian@gmail.com>
2019-07-08 08:37:06 -0700

    Remove extra comments

--- Pre-trained Classifier/get_pet_labels.py
+++ Pre-trained Classifier/get_pet_labels.py
@@ -1,29 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#                                                                             
-# PROGRAMMER: Sahar Rahmatian
-# DATE CREATED:  07/01/2019                                
-# REVISED DATE: 
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
-#          the image's filename. This function inputs: 
-#           - The Image Folder as image_dir within get_pet_labels function and 
-#             as in_arg.dir for the function call within the main function. 
-#          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main. 
-#          The results_dic dictionary has a 'key' that's the image filename and
-#          a 'value' that's a list. This list will contain the following item
-#          at index 0 : pet image label (string).
-#
-##
-# Imports python modules
 from os import listdir
 from os import path
 
-# TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
-#       with this function
-# 
+
 def get_pet_labels(image_dir):
     """
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
